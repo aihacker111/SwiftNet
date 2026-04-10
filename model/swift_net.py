06818a33d07e7e3f6b93808bef11ec4ace27ce28
aihacker111 <tinprocoder0908@gmail.com>
2026-04-11 01:07:39 +0700

    update code

--- model/swift_net.py
+++ model/swift_net.py
@@ -45,8 +45,8 @@ from torch import Tensor
 from timm.layers import trunc_normal_
 from timm.models import register_model, build_model_with_cfg
 
-from swiftnet.block import SWIFTBlock
-from swiftnet.config import SWIFTNetConfig
+from .swiftnet.block import SWIFTBlock
+from .swiftnet.config import SWIFTNetConfig
 
 
 # ---------------------------------------------------------------------------
