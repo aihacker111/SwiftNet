ca908c97bfc1d7411b4013f70ab48912b82637ff
aihacker111 <tinprocoder0908@gmail.com>
2026-04-11 01:04:55 +0700

    update code

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-NCCL_P2P_DISABLE=1 python -m torch.distributed.launch --nproc_per_node=8 --master_port 12346 --use_env main.py --model repnext_m1 --data-path ~/imagenet --dist-eval
+NCCL_P2P_DISABLE=1 python -m torch.distributed.launch --nproc_per_node=2 --master_port 12346 --use_env main.py --model swift_net_tiny --data-path /kaggle/input/datasets/mayurmadnani/imagenet-dataset --dist-eval
